8bb2f7f1e3283345f8f439c18d53d9f38ae6ba28
uch2ha <sinyavskiy.d@gmail.com>
2022-05-18 13:36:26 +0300

    Timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
 import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 import { Board } from "./models/Board";
 import { Colors } from "./models/Colors";
 import { Player } from "./models/Player";
@@ -32,6 +33,7 @@ const App = () => {
 
    return (
       <div className="app">
+         <Timer restart={restart} currentPlayer={currentPlayer} />
          <BoardComponent
             board={board}
             setBoard={setBoard}
