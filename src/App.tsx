7bfd3f77da88847f1b11c625f6180723aac75fac
uch2ha <sinyavskiy.d@gmail.com>
2022-05-18 13:23:00 +0300

    Lost Figures Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
+import LostFigures from "./components/LostFigures";
 import { Board } from "./models/Board";
 import { Colors } from "./models/Colors";
 import { Player } from "./models/Player";
@@ -37,6 +38,16 @@ const App = () => {
             currentPlayer={currentPlayer}
             swapPlayer={swapPlayer}
          />
+         <div>
+            <LostFigures
+               title="Black Figuers"
+               figures={board.lostBlackFigures}
+            />
+            <LostFigures
+               title="White Figuers"
+               figures={board.lostWhiteFigures}
+            />
+         </div>
       </div>
    );
 };
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,7 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
 import { Bishop } from "./figures/Bishop";
+import { Figure } from "./figures/Figure";
 import { King } from "./figures/King";
 import { Knight } from "./figures/Knight";
 import { Pawn } from "./figures/Pawn";
@@ -9,6 +10,8 @@ import { Rook } from "./figures/Rook";
 
 export class Board {
    cells: Cell[][] = [];
+   lostWhiteFigures: Figure[] = [];
+   lostBlackFigures: Figure[] = [];
 
    public initCells() {
       for (let i = 0; i < 8; i++) {
@@ -27,6 +30,8 @@ export class Board {
    public getBoardCopy(): Board {
       const newBoard = new Board();
       newBoard.cells = this.cells;
+      newBoard.lostWhiteFigures = this.lostWhiteFigures;
+      newBoard.lostBlackFigures = this.lostBlackFigures;
       return newBoard;
    }
 
