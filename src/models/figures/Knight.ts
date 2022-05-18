e0d451510fc2a66a42bbef58d4b3d5d1a0be8009
uch2ha <sinyavskiy.d@gmail.com>
2022-05-18 11:11:18 +0300

    Knight moving functional

--- src/models/figures/Knight.ts
+++ src/models/figures/Knight.ts
@@ -17,6 +17,9 @@ export class Knight extends Figure {
          return false;
       }
 
-      return true;
+      const dx = Math.abs(this.cell.x - target.x);
+      const dy = Math.abs(this.cell.y - target.y);
+
+      return (dx === 1 && dy === 2) || (dx === 2 && dy === 1);
    }
 }
